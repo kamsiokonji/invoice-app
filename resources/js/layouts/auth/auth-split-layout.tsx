157bfc32f3b940ba755c0a61500b2c493d2a97b8
kamsi okonji <kamsi.okonji9@gmail.com>
2025-04-06 18:34:47 +0100

    feat: complete sidebar

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -14,10 +14,6 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div className="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
                 <img src="/auth-illustration.png" alt="auth-illustration" className="absolute inset-0 h-full w-full object-cover" />
-                <Link href={route('home')} className="relative z-20 flex items-center text-lg font-medium">
-                    <img src="/logo.png" alt="logo" className="mr-2 size-8 fill-current text-white" />
-                    {name}
-                </Link>
             </div>
             <div className="w-full lg:p-8">
                 <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
