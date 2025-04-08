03c9e956b41fe59b1694857098f856f1d71fa1c4
kamsi okonji <kamsi.okonji9@gmail.com>
2025-04-08 19:18:12 +0100

    fix: update styles of invoice card

--- resources/js/components/invoice-card.tsx
+++ resources/js/components/invoice-card.tsx
@@ -12,30 +12,32 @@ interface InvoiceCardProps {
 
 export function InvoiceCard({ amount, due_date, invoice_number, status, name }: InvoiceCardProps) {
     return (
-        <Card className="border-none p-6">
-            <div className="flex items-center justify-between text-sm">
-                <div className="flex items-center justify-around gap-10">
-                    <span className="flex items-center">
+        <Card className="cursor-pointer border-none p-6">
+            <div className="flex items-start justify-between text-sm md:items-center">
+                <div className="flex flex-col items-start justify-around gap-6 md:flex-row md:items-center">
+                    <span className="flex items-center text-lg font-bold">
                         <h1 className="text-[#7e88c3]">#</h1>
                         <p>{invoice_number}</p>
                     </span>
 
                     <span className="flex items-center gap-2">
-                        <h1 className="text-[#7e88c3]">Due</h1>
-                        <p>{due_date}</p>
+                        <h1 className="text-[#7e88c3] dark:text-[#dfe3fa]">Due</h1>
+                        <p className="dark:text-[#dfe3fa]">{due_date}</p>
                     </span>
 
-                    <p className="text-[#858BB2]">{name}</p>
+                    <p className="text-[#858BB2] md:ml-9 dark:text-white">{name}</p>
                 </div>
 
-                <div className="flex items-center gap-10">
-                    <p className="font-extrabold">£ {amount.toFixed(2)}</p>
+                <div className="flex flex-col items-center gap-10 md:flex-row">
+                    <p className="text-lg font-extrabold">£ {amount.toFixed(2)}</p>
 
                     <Badge variant={status === 'Paid' ? 'success' : 'pending'} className="flex items-center gap-2 font-bold">
                         <span className={cn('h-2 w-2 rounded-full', status === 'Paid' ? 'bg-[#33D69F]' : 'bg-[#FF8F00]')} />
 
                         <span>{status}</span>
                     </Badge>
+
+                    <img src="/arrow.png" alt="right-arrrow" className="invisible md:visible" />
                 </div>
             </div>
         </Card>
--- resources/js/pages/invoices/index.tsx
+++ resources/js/pages/invoices/index.tsx
@@ -1,5 +1,6 @@
 import { InvoiceCard } from '@/components/invoice-card';
 import { Button } from '@/components/ui/button';
+import { useIsMobile } from '@/hooks/use-mobile';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
@@ -12,10 +13,11 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 export default function Invoices() {
+    const isMobile = useIsMobile();
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Invoices" />
-            <div className="mx-auto flex w-full max-w-3xl flex-col gap-18 p-4">
+            <div className="mx-auto mt-10 flex w-full max-w-3xl flex-col gap-18 p-4">
                 <div className="flex items-center justify-between">
                     <span>
                         <h1 className="text-2xl font-extrabold">Invoices</h1>
@@ -25,18 +27,16 @@ export default function Invoices() {
                     <div className="flex items-center gap-2">
                         <Button className="h-12 rounded-3xl px-3 text-xs font-bold text-white">
                             <img src="/plus.png" alt="" />
-                            New Invoice
+                            {isMobile ? 'New' : 'New Invoice'}
                         </Button>
                     </div>
                 </div>
 
                 <div className="flex flex-col gap-4">
                     {[1, 2, 3, 4, 5, 6, 7].map((_, index) => (
-                        <InvoiceCard key={index} amount={2000} due_date="2023-06-01" invoice_number={90210} status={'Pending'} name="John Doe" />
+                        <InvoiceCard key={index} amount={2000} due_date="2023-06-01" invoice_number={90210} status={'Paid'} name="John Doe" />
                     ))}
                 </div>
-
-                {/* <InvoiceCard amount={2000} due_date="2023-06-01" invoice_number={90210} status={'Pending'} name="John Doe" /> */}
             </div>
         </AppLayout>
     );
