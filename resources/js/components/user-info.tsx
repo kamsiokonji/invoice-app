f931e2189aec7ebf0552db3a438b34433582ef26
kamsi okonji <kamsi.okonji9@gmail.com>
2025-04-26 14:33:01 +0100

    feat: add deletion of items logic in the invoice controller

--- resources/js/components/user-info.tsx
+++ resources/js/components/user-info.tsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { useInitials } from '@/hooks/use-initials';
 import { type User } from '@/types';
 
@@ -8,7 +8,6 @@ export function UserInfo({ user }: { user: User }) {
     return (
         <>
             <Avatar className="h-8 w-8 overflow-hidden rounded-full">
-                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" className="rounded-full" />
                 <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
                     {getInitials(user.name)}
                 </AvatarFallback>
